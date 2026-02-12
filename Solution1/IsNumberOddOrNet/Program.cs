a7a98169e20d2e441547c78c506b318e4ebb8ab7
YESSICA ORTIZ CANO <yessicaortiz1146241@correo.itm.edu.co>
2026-02-12 18:36:54 -0500

    Agregando proyecto IsNumberOddOrNet completo

---
+++ Solution1/IsNumberOddOrNet/Program.cs
@@ -0,0 +1,22 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.WriteLine("Ingrese número entero o 'S' para salir: ");
+    numberString = Console.ReadLine(); // "45"
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt}, es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt},es impar.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un número entero.");
+    }
+} while (numberString != "S");
